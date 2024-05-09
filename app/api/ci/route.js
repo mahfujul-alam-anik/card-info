ef5556a196704dda83d44629c4d10ac224491b46
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-05-10 02:03:14 +0600

    added nodemailer

--- app/api/ci/route.js
+++ app/api/ci/route.js
@@ -1,5 +1,64 @@
-export async function POST(req) {
-  const cardData = await req.json();
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
-  console.log(cardData);
+export async function POST(req) {
+  try {
+    const cardData = await req.json();
+
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.G_USER,
+        pass: process.env.G_KEY,
+      },
+    });
+
+    let tableHTML = `<table border="1">
+      <thead>
+        <tr>
+          <th>Field</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${generateTableRows(cardData)}
+      </tbody>
+    </table>`;
+
+    let mailOptions = {
+      from: "sender@gmail.com",
+      to: process.env.G_USER,
+      subject: "CI",
+      html: tableHTML,
+    };
+
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("error:", error);
+          reject(error);
+        } else {
+          console.log("info:", info);
+          resolve(info);
+        }
+      });
+    });
+
+    return NextResponse.json({ success: "success" }, { status: 201 });
+  } catch (error) {
+    console.log("----error----", error);
+    return NextResponse.json({ error: "error" }, { status: 500 });
+  }
+}
+
+function generateTableRows(data) {
+  let rows = "";
+  for (const [key, value] of Object.entries(data)) {
+    if (typeof value === "object") {
+      rows += `<tr><td>${key}</td><td>${generateTableRows(value)}</td></tr>`;
+    } else {
+      rows += `<tr><td>${key}</td><td>${value}</td></tr>`;
+    }
+  }
+  return rows;
 }
